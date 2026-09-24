508091a7fe90a8bae1064129574aa59c582294f0
sofoklisparaskeva009 <sofoklisparaskeva009@gmail.com>
2026-09-25 01:20:07 +0300

    Render Gallery component in Index page and update header nav

---
+++ src/pages/Index.tsx
@@ -0,0 +1,8 @@
+import RythmoShowApp from "@/components/RythmoShowApp";
+import Gallery from "@/components/Gallery";
+
+export { Gallery };
+
+export default function Index() {
+  return <RythmoShowApp />;
+}
